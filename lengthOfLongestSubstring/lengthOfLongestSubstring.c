eb1f2e59865d098887de9b975796284313900506
vinod-jacob <vinodjac@live.com>
2020-03-02 21:06:07 +0530

    Improved Stats 3: Runtime =  0ms -> 100 percentile Memory  = 5.5MB -> 100 percentile
    Explanation:
    Removed the need for maintaining a substring altogether;
    afterall, the substring array was merely to make it easier to
    keep track of head and tail and thus calculate the length of a
    valid substring.

--- lengthOfLongestSubstring/lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring/lengthOfLongestSubstring.c
@@ -1,144 +1,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define HERE() //printf("%d - %s\n", __LINE__, __FUNCTION__ )
-
-bool HeadNotAtEndOfString(char ch)
-{
-    bool ret = false;
-    if (ch == '\0')
-    {
-        ret = false;
-    }
-    else
-    {
-        ret = true;
-    }
-    return ret;
-}
-
-bool ifSubstringContainsThisCharacter(char *substring, char ch, int *pos)
-{
-    bool ret = false; int i = 0;
-    for(i; substring[i] != '\0'; i++)
-    {
-        if(substring[i] == ch)
-        {
-            ret = true;
-            break;
-        }
-    }
-    *pos = i;
-    return ret;
-}
-
-void addCharacterPointedByHead(char *substring, char ch, int pos)
-{
-    /* int i = 0;
-    while(substring[i] != '\0')
-    {
-        i++;
-    } */
-    substring[pos] = ch;
-}
-
-int getCurrentLengthOfSubstring(char *substring)
-{
-    int i = 0;
-    while (substring[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
-
-void moveTailUntilDuplicateCharacterPlusOne(char *substring, char ch, int *tail)
-{
-    int i = 0;
-    while (substring[i] != ch)
-    {
-        i++;
-    }
-    *tail = *tail + i + 1;
-}
-
-void flushSubstring(char *substring)
-{
-    for (int i = 0; substring[i] != '\0'; i++)
-    {
-        substring[i] = '\0';
-    }
-}
-
-void updateSubstringUsingHeadAndTail(char *fullstring, char* substring, int head, int tail)
-{
-    int i = 0;
-    while (tail != head)
-    {
-        substring[i] = fullstring[tail];
-        i++; tail++;
-    }
-}
-
 int lengthOfLongestSubstring(char* fullString)
 {
-    char substring[128] = {'\0'};
-    int pos = 0;
-    int head = 0, tail = 0;
-    int currentLengthOfSubstring = 0;
-    int longestSubstring = 0;
-
-    int q = 0;
-
-    while(HeadNotAtEndOfString(fullString[head])) //constant time
+    int head = 0, tail = 0, pos = 0, it = 0;
+    int currentLength = 0;
+    int longest = 0;
+    bool present = false;
+    while (fullString[head] != '\0')
     {
-        //printf ("%d-------------------%s-----------------\n", q, fullString);
-        //printf("HEAD1 ---> %c === %d\n", fullString[head], head);
-        //printf("TAIL1 ---> %c === %d\n", fullString[tail], tail);
-        //printf("SUBS1 ---> %s\n", substring);
-        
+        pos = 0; present = false; //reset these guys on all iterations
+        for (it = tail; it < head; it++)
+        {
+            if(fullString[it] == fullString[head]) //checking for repetition from tail to just before head
+            {
+                present = true;
+                break;
+            }
+        }
+        pos = it; //get the position of repeated character
 
-        if ( !ifSubstringContainsThisCharacter(substring, fullString[head], &pos)) // O(N)
+        if (present)
         {
-            addCharacterPointedByHead(substring, fullString[head], pos); //O(N) --> but can be made constant time, using above step
-            currentLengthOfSubstring++;
+            longest = currentLength > longest ? currentLength : longest; //using current positions of head and tail, calculate length of substring
+            tail = pos + 1; //move tail to position just after duplicate was found
         }
-        else
-        {
-            //printf("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
-            //currentLengthOfSubstring = getCurrentLengthOfSubstring(substring); //O(N), but can be made constant time
-            longestSubstring = currentLengthOfSubstring > longestSubstring ? currentLengthOfSubstring : longestSubstring; //O(1)
-            //printf("current Tail --> %c === %d\n", fullString[tail], tail);   
-            //moveTailUntilDuplicateCharacterPlusOne(substring, fullString[head], &tail); //O(N) -> can be made O(1)
-            tail = tail + pos + 1;
-            //printf("new Tail --> %c === %d\n", fullString[tail], tail);
-            //printf("current Substring ---> %s\n", substring);
-            flushSubstring(substring); //O(1)
-            updateSubstringUsingHeadAndTail(fullString, substring, head, tail); //O(N)
-            //printf("new Substring 1 ---> %s\n", substring);
-            addCharacterPointedByHead(substring, fullString[head], head - tail); //constant time
-            currentLengthOfSubstring = head - tail + 1;
-            //printf("new Substring 2 ---> %s\n", substring);
-            //printf("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
-        }
-        head++; //constant time
-        //printf("HEAD2 ---> %c === %d\n", fullString[head], head);
-        //printf("TAIL2 ---> %c === %d\n", fullString[tail], tail);
-        //printf("SUBS2 ---> %s\n", substring);
-        //printf("----------------------------------------\n\n");
-        q++; //constant time
+        head ++;
+        currentLength = head - tail; //calculate current length at the end of each iteration
     }
-    //currentLengthOfSubstring = getCurrentLengthOfSubstring(substring); //O(N) --> can be made constant time
-    //currentLengthOfSubstring = head - tail + 1;
-    longestSubstring = currentLengthOfSubstring > longestSubstring ? currentLengthOfSubstring : longestSubstring; //O(1)
-    return longestSubstring;
+    return  longest > currentLength ? longest : currentLength;
+    
 }
 
 void main()
 {
     //char s[] = {'p','w','w','k','e','w','\0'};
     //char s[] = {'d', 'v', 'd', 'f', '\0'};
-    char s[] = {'a', 'a', 'b', 'a', 'a', 'b', '!', 'b', 'b', '\0'};
+    //char s[] = {'a', 'a', 'b', 'a', 'a', 'b', '!', 'b', 'b', '\0'};
     //char s[] = {'b', 'b', 'b', 'b', 'b'};
-    //printf("largest = %d\n", lengthOfLongestSubstring(s));
+    char s[] = {'b'};
+    printf("largest = %d\n", lengthOfLongestSubstring(s));
 }
