da1dd872ea3dd04f015a3dd81c5cf666de52d36c
vinod-jacob <vinodjac@live.com>
2020-03-06 22:35:54 +0530

    works okay for inputs null, single characters and mirror pallindromes

--- longestPalindrome/longestPalindrome.c
+++ longestPalindrome/longestPalindrome.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-char * longestPalindrome(char * s)
+char* longestPalindrome(char * s)
 {
-    int head = 0, tail = 0, it_head = 0, it_tail = 0;
-    bool isPalindrome = false;
-    int headLocation_and_length[2] = {0};
-    char* ret;
-
-    while(s[tail] != '\0')
+    int ptr = 0; int longest = 0, headposition = 0, currentlength = 0;
+    int ptr_l = 0, ptr_r = 0;
+    int i = 0;
+    char* ret = (char*)malloc(sizeof(char));
+    ret[0] = '\0';
+     while (s[ptr] != '\0')
     {
-        isPalindrome = false;
-        for(head = tail; head != '\0'; head++)
-        {
-            it_head = head; it_tail = tail;
-            if (s[head] == s[tail])
-            {
-                while(it_tail <= it_head)
-                {
-                    if(s[it_head] == s[it_tail])
-                    {
-                        isPalindrome = true;
-                    }
-                    else
-                    {
-                        isPalindrome = false;
-                        break;
-                    }
-                    it_tail++; it_head--;
-                }
-            }
-            if(isPalindrome && (tail - head + 1) > headLocation_and_length[1])
-            {
-                headLocation_and_length[0] = head;
-                headLocation_and_length[1] = tail -head + 1;
-            }
-        }
-        tail++;
-    }
-    
-}
+        ptr_l = ptr;
+        ptr_r = ptr + 1;
+        currentlength = 1;
 
+        while ((s[ptr_l] == s[ptr_r]) && (ptr_l >= 0) && (s[ptr_r] != '\0'))
+        {
+            //found a mirror palindrome
+            currentlength = (ptr_r - ptr_l) + 1;
+            if (longest < currentlength)
+            {
+                longest = currentlength;
+                headposition = ptr_l;
+            }
+            ptr_l--; ptr_r++;
+        }
+        longest = longest > currentlength ? longest : currentlength;
+        ptr++;
+    }
+    ret = realloc(ret, (longest + 1) * sizeof(char));
+    while (i < longest)
+    {
+        ret[i] = s[headposition];
+        ret[i+1] = '\0';
+        i++, headposition++;
+    }
+    return ret;
+}
 
 void main()
 {
-    char s[] = "anamalayalampoo";
-    printf
+    char s[] = "";
+    printf("%s\n",longestPalindrome(s));
 }
