755db48cfd9691be88941f9f635cddd82268d2ee
vinod-jacob <vinodjac@live.com>
2020-03-07 15:56:42 +0530

    Description: https://leetcode.com/problems/longest-palindromic-substring/
    Solution stats:
    Runtime = 16ms -> 84.20 percentile -> O(N^2)
    Memory = 5.7MB -> 100 percentile
    
    Explanantion:
    The key to solving this problem is to think of pallindromes as characters
    that mirror each other.
    In that sense, we have two kinds of pallindromes - pivots and mirrors.
    Pivots have a character whose either sides have same characters. (odd number of characters in these pallindromes)
    Mirrors are pallindromes that lack a pivot (even number of characters)
    
    We check for both conditions for each cycle, coz inputs like "ccc" and "cccc"
    can throw us off. Therefore, we cannot afford a if -else if statement.

--- longestPalindrome/longestPalindrome.c
+++ longestPalindrome/longestPalindrome.c
@@ -7,29 +7,57 @@ char* longestPalindrome(char * s)
     int ptr = 0; int longest = 0, headposition = 0, currentlength = 0;
     int ptr_l = 0, ptr_r = 0;
     int i = 0;
-    char* ret = (char*)malloc(sizeof(char));
-    ret[0] = '\0';
+    char* ret;
      while (s[ptr] != '\0')
     {
-        ptr_l = ptr;
-        ptr_r = ptr + 1;
-        currentlength = 1;
+        currentlength = 1; //each time a character is not null, we have a single character palindrome
 
-        while ((s[ptr_l] == s[ptr_r]) && (ptr_l >= 0) && (s[ptr_r] != '\0'))
-        {
-            //found a mirror palindrome
-            currentlength = (ptr_r - ptr_l) + 1;
-            if (longest < currentlength)
+        /*first condition to ensure we don't fall off the earth walking backwards.
+         We don't check to see if we fall off walking forwards, coz atleast there is
+         going to be a null character there to stop us*/
+        if ((ptr-1 >= 0 ) && (s[ptr - 1] == s[ptr + 1]))
+        { //found a pivot point
+            ptr_l = ptr - 1;
+            ptr_r = ptr + 1;
+
+            /*making the check to see if ptr_l is greater than 0 first, so that
+            we don't create a runtime error by accessing s[-1] (conditional short-circuiting
+            helps us out)*/
+            while ((ptr_l >= 0) && (s[ptr_r] != '\0') && (s[ptr_l] == s[ptr_r]))
             {
-                longest = currentlength;
-                headposition = ptr_l;
+                currentlength = (ptr_r - ptr_l) + 1;
+                if (longest < currentlength)
+                {
+                    longest = currentlength;
+                    headposition = ptr_l;
+                }
+                ptr_l--; ptr_r++;
             }
-            ptr_l--; ptr_r++;
+        } //end pivot pallindrome case
+
+        if (s[ptr] == s[ptr + 1]) //mirror pallindrom case
+        {
+            ptr_l = ptr;
+            ptr_r = ptr + 1;
+
+            while ((ptr_l >= 0) && (s[ptr_r] != '\0') && (s[ptr_l] == s[ptr_r]))
+            {
+                currentlength = (ptr_r - ptr_l) + 1;
+                if (longest < currentlength)
+                {
+                    longest = currentlength;
+                    headposition = ptr_l;
+                }
+                ptr_l--; ptr_r++;
+            }
         }
-        longest = longest > currentlength ? longest : currentlength;
         ptr++;
     }
-    ret = realloc(ret, (longest + 1) * sizeof(char));
+    longest = longest > currentlength ? longest : currentlength; //this to catch those input strings that are only a single character
+    ret = (char*)malloc((longest+1) * sizeof(char)); //longest + 1 to include the null character at the end
+    
+    /*populate ret with our longest pallindromic substring*/
+    ret[0] = '\0';
     while (i < longest)
     {
         ret[i] = s[headposition];
@@ -41,6 +69,6 @@ char* longestPalindrome(char * s)
 
 void main()
 {
-    char s[] = "";
+    char s[] = "cbbd";
     printf("%s\n",longestPalindrome(s));
 }
