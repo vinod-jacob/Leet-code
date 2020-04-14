a5f1f30a2e938b3f6c3249906454a6b7ae434509
vinod-jacob <vinodjac@live.com>
2020-04-14 20:54:12 +0530

    Desc: https://leetcode.com/problems/3sum multimap implementation Time linit exceeded: 269/313

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 using namespace std;
 
@@ -9,37 +9,42 @@ using namespace std;
         vector<vector<int>> threeSum(vector<int>& nums)
         {
             vector<vector<int>> sum3;
-            vector<int>::iterator i, j, k;
-            for (i = nums.begin(); i != nums.end(); i++)
+            multimap<int, int> umap;
+            for (int i = 0; i < nums.size(); i++)
             {
-                for (j = i + 1; j != nums.end(); j++)
+                for (auto j = umap.begin(); j != umap.end(); j++)
                 {
-                    for(k = j + 1; k != nums.end(); k++)
+                    vector<int> triplet;
+                    int target = - (j->first + nums[i]);
+                    multimap<int, int>::iterator found = umap.find(target);
+                    if(found != umap.end())
                     {
-                        if ((*i+ *j + *k) == 0)
-                        {   
-                            vector<int> triplets;
-                            triplets.push_back(*i);
-                            triplets.push_back(*j);
-                            triplets.push_back(*k);
-                            sort(triplets.begin(), triplets.end());
-                            bool f = false;
-                            for(auto q = sum3.begin(); q != sum3.end(); q++)
-                            {                               
-                                if (*q == triplets)
+                        if (j->second != found->second) //make sure you don't find yourself,babe
+                        {
+                            triplet.push_back(nums[i]);
+                            triplet.push_back(j->first);
+                            triplet.push_back(target);
+                            sort(triplet.begin(), triplet.end());
+                            bool dup = false;
+                            for (auto k = sum3.begin(); k != sum3.end(); k++)
+                            {
+                                if (*k == triplet)
                                 {
-                                    f = true;
+                                    dup = true;
                                     break;
                                 }
                             }
-                            if (!f)
+                            if(!dup)
                             {
-                                sum3.push_back(triplets);
+                                sum3.push_back(triplet);
                             }
-                            
                         }
+                        
                     }
                 }
+                pair<int,int> numAndInd;
+                numAndInd.first = nums[i]; numAndInd.second = i;
+                umap.insert(numAndInd);
             }
             return sum3;
         }
@@ -47,7 +52,8 @@ using namespace std;
 
     int main()
     {
-        vector<int> a = {-1, 0, 1, 2, -1, -4, 44, 0, -44, 1};
+        //vector<int> a = {-1, 0, 1, 2, -1, -4, 44, 0, -44, 1, 0};
+        vector<int> a = {0,0,0};
         Solution S;
         vector<vector<int>> result =  S.threeSum(a);
         for(auto i = result.begin(); i != result.end(); i++)
