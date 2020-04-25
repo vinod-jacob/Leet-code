9228bdb07e2a05ee8aa84e926ec41507b08d1720
vinod-jacob <vinodjac@live.com>
2020-04-26 00:45:39 +0530

    Desc: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
    Stats:
    Runtime : 0ms -> 100%
    Memory: 6.4Mb -> 100%
    
    Strategy used: simple recursion

---
+++ letterCombPhNum/letterComb.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+class Solution {
+    map <char, string> tel = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, \
+        {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        vector<string> res;
+        string prefix = "";
+        recurse(prefix, digits, res);
+        return res;
+    }
+
+    void recurse(string prefix, string digits, vector<string> &res)
+    {
+        if (digits.size() != 0)
+        {
+            char d = digits[0];
+            string decremented_digits = digits.substr(1, digits.size()-1);
+            string letters = tel[d];
+            for (int i = 0; i<letters.size(); i++)
+            {
+                recurse(prefix + letters[i], decremented_digits, res);
+            }
+        }
+        else if (digits.size() == 0)
+        {
+            res.push_back(prefix);
+        }
+    }
+};
+
+int main()
+{
+    Solution S1;
+    vector<string> ans = S1.letterCombinations("23");
+    for (auto i = ans.begin(); i != ans.end(); i++)
+    {
+        cout << *i << "\n";
+    }
+}
