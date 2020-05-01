6704c43057c9815423fa0053b7424452a4ff0c39
vinod-jacob <vinodjac@live.com>
2020-05-01 13:44:28 +0530

    Desc: https://leetcode.com/problems/generate-parentheses/
    Stats:
    Runtime: 12ms -> 21.8%
    Memory: 7.7 MB -> 100%

--- 22.generateParanthesis/generateParanthesis.cpp
+++ 22.generateParanthesis/generateParanthesis.cpp
@@ -15,65 +15,37 @@ public:
         int N = 2*n;
         string prefix = "";
         vector<string> allPermuts;
-        genAllPar(prefix, N, allPermuts);
-        return filterValid(allPermuts);
+        genAllPar(prefix, N, 0, allPermuts);
+        return allPermuts;
+
     }
 
-    void genAllPar(string prefix, int n,  vector<string> &allPermuts)
+    void genAllPar(string prefix, int n, int score, vector<string> &allPermuts)
     {
         if(n != 0)
         {
             for(int i = 0; i < 2; i++)
             {
                 char ch = brackets[i];
-                genAllPar(prefix + ch, n-1, allPermuts);
+                int tracker = score;
+                ch == '('? tracker++ : tracker--;
+                if (tracker < 0) break;
+                genAllPar(prefix + ch, n-1, tracker, allPermuts);
             }
         }
-        else if(n == 0)
+        else if(n == 0 && score == 0)
         {
             allPermuts.push_back(prefix);
         }
     }
 
-    vector<string> filterValid(vector<string> &allPermuts)
-    {
-        vector<string> ans;
-        for(auto i = allPermuts.begin(); i != allPermuts.end(); i++)
-        {   
-            string eachPermut = *i;
-            bool valid = true;
-            stack<char> s;
-            for(int j = 0; j < eachPermut.size(); j++)
-            {
-                char ch = eachPermut[j];
-                if (ch == '(')
-                {
-                    s.push(ch);
-                }
-                else if (!s.empty())
-                {
-                    s.pop();
-                }
-                else if(s.empty())
-                {
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid && s.empty())
-            {
-                ans.push_back(*i);
-            }
-        }
-        return ans;
-    } 
 };
 
 int main()
 {
     Solution s1;
     vector<string> ans;
-    ans = s1.generateParenthesis(0);
+    ans = s1.generateParenthesis(12);
 
     for (auto i = ans.begin(); i != ans.end(); i++)
     {
