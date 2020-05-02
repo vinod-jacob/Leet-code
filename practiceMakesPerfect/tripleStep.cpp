efbb1e9c87f7510695ddaaa5f4949eea3b819da6
vinod-jacob <vinodjac@live.com>
2020-05-02 21:14:14 +0530

    Desc: https://github.com/careercup/CtCI-6th-Edition-cpp/blob/a68ba3e1c630a4d218ff1294f3eaf5aeced449ec/chapter-8-recursion-and-Dynamic-Programming/8-1-Triple-Step.cpp
    Dynamic programming solution and Naive recursive solution

--- practiceMakesPerfect/tripleStep.cpp
+++ practiceMakesPerfect/tripleStep.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int tripleStep_dp(int n, int* a)
+unsigned long long tripleStep_dp(int n, unsigned long long* a)
 {
     if (a[n] != 0)
         return a[n];
     else
     {
         a[n] = tripleStep_dp(n-1, a) + tripleStep_dp(n-2, a) + tripleStep_dp(n-3, a);
-        cout << "n = " << n << "    a[n] = " << a[n] <<endl;
+        // cout << "n = " << n << "    a[n] = " << a[n] <<endl;
         return a[n];
     }
     
 }
 
-int triplestep_naive(int n)
+unsigned long long triplestep_naive(int n)
 {
     if (n <=0)
         return 0;
@@ -26,7 +27,7 @@ int triplestep_naive(int n)
         return 4;
     else
     {
-        cout << "n = "<< n << endl;
+        // cout << "n = "<< n << endl;
         return triplestep_naive(n-1) + triplestep_naive(n-2) + triplestep_naive(n-3);
     }
     
@@ -34,12 +35,11 @@ int triplestep_naive(int n)
 
 int main()
 {
-    int n = 7 ;
-    int *a = new int[n+1];
+    int n = 37 ;
+    unsigned long long *a = new unsigned long long[n+1];
     a[0] = 0; a[1] = 1; a[2] = 2; a[3] = 4; 
-    cout << "answer dp  = \n-----------------\n" << tripleStep_dp(n, a) << endl;
-
-    cout <<"ans naive = \n--------------------\n" << triplestep_naive(n) << endl;
+    cout << "answer dp  = " << tripleStep_dp(n, a) << endl;
+    cout <<"ans naive = " << triplestep_naive(n) << endl;
 }
 
 
