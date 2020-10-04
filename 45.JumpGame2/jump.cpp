7fa7296897dd18d275822b562432715590d9a9d5
vinod-jacob <vinodjac@live.com>
2020-10-04 11:27:10 +0530

    Desc: https://leetcode.com/problems/jump-game-ii/
    Runtime: 12 ms, faster than 99.51%
    Memory Usage: 13.4 MB, less than 37.49%

--- 45.JumpGame2/jump.cpp
+++ 45.JumpGame2/jump.cpp
@@ -11,46 +11,35 @@ public:
         int size = nums.size();
         if (size == 0 || size == 1) return 0;
 
-        jump(0, count, nums);
-        return count;
-    }
-
-    void jump(int index, int& count, vector<int>& nums)
-    {
-        if(index != nums.size()-1)
+        int tojump = 1;
+        for(int index = 0; index < size-1; index = index + tojump)
         {
-            /* At each location, we try to make a jump that will
-            take us the most far, unless any jump will take us to the final destination itself*/
-
-            int val = nums[index];
-            int combinedmaxjump = 2; /*2 is the minimum length we can jump with 2 elements*/
-            int tojump = 1;
-
-            for(int jumplen = 1; jumplen <= val; jumplen++)
+            int maxjump = 2;
+            for(int jumplen = 1; jumplen <= nums[index]; jumplen++)
             {
-                if(index + jumplen == nums.size() -1)
+                if (jumplen + index == size-1)
                 {
                     tojump = jumplen;
                     break;
                 }
                 else
                 {
-                    if (jumplen + nums[index + jumplen] > combinedmaxjump)
+                    if ((jumplen + index < size) && (jumplen + nums[jumplen + index] >= maxjump))
                     {
-                        combinedmaxjump = jumplen + nums[index + jumplen];
                         tojump = jumplen;
+                        maxjump = jumplen + nums[jumplen + index];
                     }
                 }
             }
             count++;
-            Solution::jump(index+tojump, count, nums);
         }
+        return count;
     }
 };
 
 int main()
 {
-    vector<int> nums = {2,3,1,1,4};
+    vector<int> nums = {2,1,1,1,1};
     Solution S1;
     cout << S1.jump(nums) << endl;
 }
