f70b85770cd45e31ede904b40a3764e1b201245e
vinod-jacob <vinodjac@live.com>
2020-11-14 20:12:03 +0530

    Desc: https://leetcode.com/problems/maximum-subarray
    Starategy: Kadane's algo (Dynamic programming)
    
    Runtime: 12 ms, faster than 69.70%
    Memory Usage: 13.4 MB, less than 48.73%

--- 53.MaximumSubarray/maxsub.cpp
+++ 53.MaximumSubarray/maxsub.cpp
@@ -8,45 +8,20 @@ public:
     int maxSubArray(vector<int>& nums)
     {
         int retsum = nums[0];
-        int pcsum = nums[0];
-
+        int prevsum = nums[0];
         for(int i = 1; i < nums.size(); i++)
         {
-            int isum = pcsum + nums[i];
-
-            if(isum > retsum)
-            {
-                if(nums[i] > retsum && isum < nums[i])
-                {
-                    retsum = nums[i];
-                    pcsum = nums[i];
-                }
-                else
-                {
-                    pcsum = isum;
-                    retsum = isum;
-                }
-            }
-            else if (nums[i] > retsum)
-            {
-                pcsum = nums[i];
-                retsum = nums[i];
-            }
-            else
-            {
-                pcsum = isum;
-            }
-
+            prevsum = max(prevsum + nums[i], nums[i]);
+            retsum = max(retsum, prevsum);
         }
         return retsum;
     }
-
 };
 
 int main()
 {
-    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
-    // vector<int> nums = {8,-19,5,-4,20};
+    // vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> nums = {8,-19,5,-4,20};
     // vector<int> nums = {-2,-1};
     Solution S1;
     cout << S1.maxSubArray(nums);
