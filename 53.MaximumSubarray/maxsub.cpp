de390f532bcdb8db6de767a3d1d7f173fc090ac9
vinod-jacob <vinodjac@live.com>
2020-11-14 19:55:58 +0530

    Desc: https://leetcode.com/problems/maximum-subarray
    Pass: 181 / 202

---
+++ 53.MaximumSubarray/maxsub.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int retsum = nums[0];
+        int pcsum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            int isum = pcsum + nums[i];
+
+            if(isum > retsum)
+            {
+                if(nums[i] > retsum && isum < nums[i])
+                {
+                    retsum = nums[i];
+                    pcsum = nums[i];
+                }
+                else
+                {
+                    pcsum = isum;
+                    retsum = isum;
+                }
+            }
+            else if (nums[i] > retsum)
+            {
+                pcsum = nums[i];
+                retsum = nums[i];
+            }
+            else
+            {
+                pcsum = isum;
+            }
+
+        }
+        return retsum;
+    }
+
+};
+
+int main()
+{
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    // vector<int> nums = {8,-19,5,-4,20};
+    // vector<int> nums = {-2,-1};
+    Solution S1;
+    cout << S1.maxSubArray(nums);
+}
